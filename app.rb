21bfdb5a62b0757a80859997f2f01ebfaa70ad9d
Oluwapelumi Egunjobi <pelumitegunjobi@gmail.com>
2018-07-18 13:09:36 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,11 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do
+    "This is a homepage!!!!"
+  end
 
   get '/food_form' do
     erb :food_form
   end
 
+  post '/food' do
+   "My name is #{params[:name]}, and I love #{params[:favourite_food]}"
+  end
   # Add your post route and action below
 
 end
